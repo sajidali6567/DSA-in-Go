69e7291f2fdb047342c9d29c97b99aedc1a962fd
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2023-07-08 12:02:18 +0530

    Time: 2 ms (61.28%), Space: 2 MB (83.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.go
+++ 0075-sort-colors/0075-sort-colors.go
@@ -4,21 +4,20 @@ func sortColors(nums []int)  {
     // j will point to the position of next two
     // k will keep moving and swapping 0 and 2 to tha first and last of array
     i, j, k := 0, len(nums)-1, 0
+    swap := func(i, j int) {
+        nums[i], nums[j] = nums[j], nums[i]
+    }
     for k <= j {
         switch nums[k] {
         case 0:
-            nums[i], nums[k] = nums[k], nums[i]
+            swap(i, k)
             i++
             k++
         case 1:
             k++;
         case 2:
-            nums[k], nums[j] = nums[j], nums[k]
+            swap(j, k)
             j--
         }
     }
 }
-
-// func swap(nums []int, i, j int) {
-//     nums[i], nums[j] = nums[j], nums[i]
-// }
