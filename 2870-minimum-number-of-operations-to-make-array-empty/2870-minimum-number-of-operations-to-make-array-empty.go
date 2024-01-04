588e6b60a195abc6b85e3e85b929465147045ae7
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2024-01-04 22:11:36 +0530

    Time: 95 ms (96.30%), Space: 9.4 MB (74.07%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.go
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.go
@@ -1,3 +1,11 @@
+// Observations:
+// 1. Any number other than 1 can be written in combination of 2 and 3.
+// 2. If we pick any number n (other than 1), either n, n-2, n-4 will be divisible by 3
+// Approach 2:
+// if we divide frequency by 3, we will get remainder as 0, 1, 2
+// in case of 2, frequency/3 + 1 (pair of 2)
+// in case of 1, take 3 out to make two pairs of 2
+// frequency/3 - 1((pair of 3) + 2 (two pairs of 2) == frequency/3 + 1
 func minOperations(nums []int) int {
     frequencyMap := make(map[int]int, 0)
     for _, num := range nums {
@@ -5,15 +13,14 @@ func minOperations(nums []int) int {
     }
     minOpRequired := 0
     for _, frequency := range frequencyMap {
-        switch {
-            case frequency == 1:
-                return -1
-            case frequency % 3 == 0:
+        if frequency == 1 {
+            return -1
+        }     
+        switch frequency % 3 {
+            case 0:
                 minOpRequired += frequency/3
-            case (frequency - 2) % 3 == 0:
-                minOpRequired += (frequency - 2) / 3 + 1
-            case (frequency - 4) % 3 == 0:
-                minOpRequired += (frequency -4) / 3 + 2
+            default: // case 1 and 2
+                minOpRequired += frequency/3 + 1
         }
     }
     return minOpRequired
