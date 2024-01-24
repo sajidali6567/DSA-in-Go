b0d61fda54849b9806339da63c852882867ce824
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2024-01-24 22:58:37 +0530

    Time: 259 ms (75.87%), Space: 177 MB (99.46%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -14,21 +14,19 @@ public:
     map<int, int> mp;
     int count = 0;
     void DFS(TreeNode* root) {
-        if(root == NULL) return;
         mp[root->val]++; // add the root node to the map
         if(root->left == NULL && root->right == NULL) { // leaf node
             checkForPalindrome();
         }
-        DFS(root->left);
-        DFS(root->right);
+        if(root->left) DFS(root->left);
+        if(root->right) DFS(root->right);
         mp[root->val]--; // remove once we are done with the left and right of it
     }
+    // there should be only one or zero number with odd frequency to make it palindrome
     void checkForPalindrome() {
-        // cout << "************\n";
         int oddCount = 0;
         for(auto it:mp) {
-            // cout << it.first << " - > " << it.second << endl;
-            if (it.second%2 != 0) oddCount++;
+            if (it.second & 1) oddCount++;
         }
         if(oddCount <= 1) count++;
     }
