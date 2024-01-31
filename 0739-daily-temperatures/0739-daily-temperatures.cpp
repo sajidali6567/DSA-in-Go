0f31737b83ddcf5503acda17f1081aa8c79c4797
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2024-01-31 23:33:47 +0530

    Time: 111 ms (96.36%), Space: 106.3 MB (24.14%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<pair<int, int>> stack;
+        stack<int> stack;
         vector<int> ans;
         int n = temperatures.size();
         // start from end, and maintain the monotonic stack of maximum elements
         for(int i = n-1; i>=0; i--) {
             int temp = temperatures[i];
-            while(!stack.empty() && temp >= stack.top().first) {
+            while(!stack.empty() && temp >= temperatures[stack.top()]) {
                 stack.pop();
             }
-            if(stack.empty()) ans.push_back(0); else ans.push_back({stack.top().second - i});
-            stack.push({temp, i});
+            if(stack.empty()) ans.push_back(0); else ans.push_back(stack.top() - i);
+            stack.push(i);
         }
         reverse(ans.begin(), ans.end());
         return ans;
