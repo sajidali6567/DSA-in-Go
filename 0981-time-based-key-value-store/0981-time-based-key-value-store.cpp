7b79bf7fe11644978735e6f424cb42dc1b5b0458
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2024-02-24 16:00:16 +0530

    Time: 256 ms (89.89%), Space: 133.3 MB (51.61%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -5,27 +5,34 @@ public:
     }
     
     void set(string key, string value, int timestamp) {
-        pair<int, string> cacheValue = make_pair(timestamp, value);
-        cache[key].push_back(cacheValue);
+        cache[key].push_back({timestamp, value});
     }
     
     string get(string key, int timestamp) {
-        if(cache[key].size() == 0) return "";
+        string value; // empty string
+        if(cache[key].size() == 0) return value;
         
-        // do binary search and find the maximum element <= timestamp
         int low = 0, high = cache[key].size()-1;
+        int index = binarySearch(low, high, timestamp, cache[key]);
+        
+        if(index == -1) return value;
+        return cache[key][index].second;
+    }
+    
+    // do binary search and find the index of maximum element <= timestamp
+    // -1 if not found
+    int binarySearch(int low, int high, int timestamp, vector<pair<int, string>>& cacheValues) {
         int index = -1;
         while(low <= high) {
             int mid = low + (high-low)/2;
-            if(cache[key][mid].first <= timestamp) {
+            if(cacheValues[mid].first <= timestamp) {
                 index = mid;
                 low = mid+1;
             } else {
                 high = mid-1;
             }
         }
-        if(index == -1) return "";
-        return cache[key][index].second;
+        return index;
     }
 };
 
