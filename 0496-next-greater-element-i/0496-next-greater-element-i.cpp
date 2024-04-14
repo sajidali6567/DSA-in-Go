0f7f9163695708468d9136d08295db1571119c82
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2024-04-14 21:04:23 +0530

    Time: 4 ms (72.54%), Space: 11.5 MB (57.71%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,35 +2,27 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         map<int, int> mp; // maps element of nums2 to its original index
-        vector<int> ogNums2(nums2.begin(), nums2.end()); // original nums2 array
+        vector<int> nextGreaterElem(nums2.size(), -1); // remaining elements of stack does not have any next greater element so initialing all of them as -1
+        
         
         stack<int> st; // stores index of the array
-        for(int i=0;i<nums2.size();i++) {
+        for(int i=0;i<nextGreaterElem.size();i++) {
             mp[nums2[i]] = i;
             // update current index as next greater index for all the index at which elements are less than current element
             while(!st.empty() && nums2[i] > nums2[st.top()] ) {
-                nums2[st.top()] = i;
+                nextGreaterElem[st.top()] = nums2[i];
                 st.pop();
             }
             
             st.push(i);
         }
         
-        // remaining elements of stack does not have any next greater element to push as -1
-        while(!st.empty()) {
-            nums2[st.top()] = -1;
-            st.pop();
-        }
-        
-        // index of the element in nums2 can be found using mp[num]
-        // nums2 contains the index of next greater element
-        // use original nums2 to find the next greater element using index found in the previous step
+        // index of the element in nextGreaterElem can be found using mp[num]
+        // nextGreaterElem contains the index of next greater element
+        // use original nextGreaterElem to find the next greater element using index found in the previous step
         vector<int> ans;
         for(auto num:nums1) {
-            if(nums2[mp[num]] == -1) {
-                ans.push_back(-1); continue;
-            }
-            ans.push_back(ogNums2[nums2[mp[num]]]);
+            ans.push_back(nextGreaterElem[mp[num]]);
         }
         
         return ans;
