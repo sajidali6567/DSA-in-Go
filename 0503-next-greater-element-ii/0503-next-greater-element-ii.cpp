950053b451cf9644e99f92f4d3cb33541ca178d8
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2024-04-14 21:22:05 +0530

    Time: 62 ms (9.63%), Space: 32.2 MB (5.15%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    
-    void nge1(map<int, int>& mp, vector<int>& nextGreaterElem, stack<int>& st, vector<int>& nums) {
-        for(int i=0;i<nextGreaterElem.size();i++) {
-            mp[nums[i]] = i;
-            // update current index as next greater index for all the index at which elements are less than current element
-            while(!st.empty() && nums[i] > nums[st.top()] ) {
-                nextGreaterElem[st.top()] = nums[i];
-                st.pop();
-            }
-            st.push(i);
-        }
-    }
-    
     vector<int> nextGreaterElements(vector<int>& nums) {
         map<int, int> mp; // maps element of nums2 to its original index
-        vector<int> nextGreaterElem(nums.size(), -1); // remaining elements of stack does not have any next greater element so initialing all of them as -1
+        int n = nums.size();
+        vector<int> nextGreaterElem(n, -1); // remaining elements of stack does not have any next greater element so initialing all of them as -1
         stack<int> st; // stores index of the array
         // run the next greater element I code two time
-        nge1(mp, nextGreaterElem, st, nums);
-        nge1(mp, nextGreaterElem, st, nums);
+        for(int i=0;i< 2*n -1 ;i++) {
+            mp[nums[i%n]] = i%n;
+            // update current index as next greater index for all the index at which elements are less than current element
+            while(!st.empty() && nums[i%n] > nums[st.top()] ) {
+                nextGreaterElem[st.top()] = nums[i%n];
+                st.pop();
+            }
+            st.push(i%n);
+        }
         
         return nextGreaterElem;
     }
