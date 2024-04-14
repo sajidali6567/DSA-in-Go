a984f7a809500144940ef30d01c455dcc5a53c1c
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2024-04-14 21:18:13 +0530

    Time: 58 ms (9.94%), Space: 32.1 MB (5.15%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& nums2) {
+    
+    void nge1(map<int, int>& mp, vector<int>& nextGreaterElem, stack<int>& st, vector<int>& nums) {
+        for(int i=0;i<nextGreaterElem.size();i++) {
+            mp[nums[i]] = i;
+            // update current index as next greater index for all the index at which elements are less than current element
+            while(!st.empty() && nums[i] > nums[st.top()] ) {
+                nextGreaterElem[st.top()] = nums[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+    }
+    
+    vector<int> nextGreaterElements(vector<int>& nums) {
         map<int, int> mp; // maps element of nums2 to its original index
-        vector<int> nextGreaterElem(nums2.size(), -1); // remaining elements of stack does not have any next greater element so initialing all of them as -1
-        
-        
+        vector<int> nextGreaterElem(nums.size(), -1); // remaining elements of stack does not have any next greater element so initialing all of them as -1
         stack<int> st; // stores index of the array
-        for(int i=0;i<nextGreaterElem.size();i++) {
-            mp[nums2[i]] = i;
-            // update current index as next greater index for all the index at which elements are less than current element
-            while(!st.empty() && nums2[i] > nums2[st.top()] ) {
-                nextGreaterElem[st.top()] = nums2[i];
-                st.pop();
-            }
-            
-            st.push(i);
-        }
-        
-        for(int i=0;i<nextGreaterElem.size();i++) {
-            mp[nums2[i]] = i;
-            // update current index as next greater index for all the index at which elements are less than current element
-            while(!st.empty() && nums2[i] > nums2[st.top()] ) {
-                nextGreaterElem[st.top()] = nums2[i];
-                st.pop();
-            }
-            
-            st.push(i);
-        }
+        // run the next greater element I code two time
+        nge1(mp, nextGreaterElem, st, nums);
+        nge1(mp, nextGreaterElem, st, nums);
         
         return nextGreaterElem;
     }
